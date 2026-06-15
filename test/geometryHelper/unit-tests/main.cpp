9914c76f98c34a7a76544d5236d918275bc5dc37
Illia Denisov <robbinnegood@gmail.com>
2026-06-15 19:47:18 +0300

    Divide testing in different categories

--- test/geometryHelper/unit-tests/main.cpp
+++ test/geometryHelper/unit-tests/main.cpp
@@ -1,7 +1,6 @@
-#include "../src/geometryHelper/include/PointHelper.h"
-#include "../src/geometryHelper/include/CollisionHelper.h"
+#include "../../../src/geometryHelper/include/CollisionHelper.h"
+#include "../../../src/geometryHelper/include/PointHelper.h"
 #include <catch2/catch_test_macros.hpp>
-#include <benchmark/benchmark.h>
 
 TEST_CASE( "Point in rectangle hit testing", "[point_in_rect]" ) {
     CRect rect{0, 0, 1, 1};
@@ -20,20 +19,3 @@ TEST_CASE( "Point in rectangle hit testing", "[point_in_rect]" ) {
     point = PointDouble{1.0, 1.0};
     REQUIRE(CollisionHelper::IsPointInRectangle(point, rect));
 }
-
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-
-BENCHMARK(BM_StringCopy);
-BENCHMARK_MAIN();
