2083ee29747b5a8d30e913c19f68c382037f3b6a
Illia Denisov <robbinnegood@gmail.com>
2026-06-16 19:55:27 +0300

    Fix main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include "src/geometryHelper/include/PointHelper.h"
-#include "src/geometryHelper/include/CollisionHelper.h"
+#include <meta>
+#include <vector>
 
-int main()
-{
-    auto isInRect = CollisionHelper::IsPointInRectangle(PointDouble{0.0, 0.0}, CRect{-1, -1, 1, 1});
-    int a[10];
-    std::cout << a[15] << "\n";
-    return 0;
+int main() {
+
 }
